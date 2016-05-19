eb11ecee8c98122ae71d589213b54fad214cbb31
Gaurav Sood <gsood07@gmail.com>
2016-05-19 16:35:32 -0400

    get alexa data works

--- rdomains/R/get_alexa_data.R
+++ rdomains/R/get_alexa_data.R
@@ -1,12 +1,22 @@
 #' Get Alexa Data
 #'
-#' @param domain domain name
-#' @param apikey virustotal API key
+#' @param outdir    Optional; folder to which you want to save the file; Default is same folder
+#' @param overwrite Optional; default is FALSE
 #' 
-#' @return data frame
 #'  
 #' @export
-#' @references \url{https://docs.aws.amazon.com/AlexaWebInfoService/latest/index.html?ApiReference_UrlInfoAction.html}
+#' @references \url{https://support.alexa.com/hc/en-us/articles/200461990-Can-I-get-a-list-of-top-sites-from-an-API-}
 #' @examples \dontrun{
-#' alexa_cat(domain="http://www.google.com")
+#' get_alexa_cat()
 #' }
+
+get_alexa_data <- function(outdir=".", overwrite=FALSE) {
+
+	tmp <- tempfile()
+	curl_download("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip", tmp)
+	unzip(tmp, exdir=outdir, overwrite=overwrite)
+	unlink(tmp)
+
+	cat("Alexa Top 1M Domains saved to the following destination:", outdir, "\n")
+
+}
