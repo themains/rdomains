f0c129272d23b7fada93ef4815c3408b0711075b
***** <gsood07@gmail.com>
2016-10-27 09:21:05 -0400

    fixes issue soodoku/domain_classifier/#4

--- rdomains/R/virustotal_cat.R
+++ rdomains/R/virustotal_cat.R
@@ -1,7 +1,8 @@
 #' Get Category from Virustotal
 #'
 #' Returns category of content from 6 major services including: BitDefender, Dr. Web, Alexa (DMOZ), Google, 
-#' Websense, and Trendmicro
+#' Websense, and Trendmicro. Not all services will have categories for all the domains. When the categories are
+#' not returned for a particular domain, we return a NA.
 #' 
 #' Get the API Access Key from \url{http://www.virustotal.com/}. Either pass the API Key to the function 
 #' or set the environmental variable: \code{VirustotalToken}. Environment variables persist within 
@@ -31,22 +32,26 @@ virustotal_cat <- function(domain = NULL, apikey=NULL) {
   res <-  domain_report(domain)
 
   # Companies from which virustotal returns domain categories 
-  cat_names <- c("BitDefender category", "Dr.Web category", "Alexa category", "categories", "Websense ThreatSeeker category", "TrendMicro category")
-
-  # Return data.frame
-  res_df_names <- c("bitdefender", "dr_web", "alexa", "google", "websense", "trendmicro")
+  cat_names <- c("BitDefender category" = "bitdefender", 
+                 "Dr.Web category" = "dr_web", 
+                 "Alexa category" ="alexa", 
+                 "categories" ="google", 
+                 "Websense ThreatSeeker category" ="websense", 
+                 "TrendMicro category"="trendmicro")
 
   # If domain not found, return a data.frame with domain name + NAs
   if (res$verbose_msg=="Domain not found") {
-      d_res     <- read.table(text = "", col.names=res_df_names)
+      d_res     <- read.table(text = "", col.names=cat_names)
       d_res[1,] <- NA
   } else {
     # If results are returned
     # do.call(rbind, lapply(sapply(res[which(names(res) %in% cat_names)], cbind), as.data.frame)) (multiple cat by Google)
-    d_res <- as.data.frame(do.call(cbind, sapply(res[which(names(res) %in% cat_names)], "[", 1)))
-    names(d_res)[names(d_res) %in% cat_names] <- res_df_names
+    d_res <- as.data.frame(do.call(cbind, sapply(res[which(names(res) %in% names(cat_names))], "[", 1)))
+    names(d_res) <- cat_names[match(names(d_res), names(cat_names))]
+    d_res[,cat_names[!(cat_names %in% names(d_res))]] <- NA
+    d_res <- d_res[,names(d_res)[order(cat_names)]] # Reorder so that we always get same order of columns
   }
-  
+
   # Return
   data.frame(domain, d_res)         
   
