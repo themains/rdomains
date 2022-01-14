27aadacda89614514bade564459692c27ee260e2
***** <gsood07@gmail.com>
2022-01-14 15:51:55 -0800

    update shallalist

--- data-raw/shallalist/shall_downloaded.R
+++ data-raw/shallalist/shall_downloaded.R
@@ -15,13 +15,13 @@ curl_download("https://raw.githubusercontent.com/cbuijs/shallalist/master/violen
 
  
 # read_lines() to read one line at a time
-ads = data.frame(domains = read_lines("ads"), cat = "ads")
-gambling = data.frame(domains = read_lines("gambling"), cat = "gambling")
-finance = data.frame(domains = read_lines("finance"), cat = "finance")
-porn = data.frame(domains = read_lines("porn"), cat = "porn")
-spyware = data.frame(domains = read_lines("spyware"), cat = "spyware")
-tracker = data.frame(domains = read_lines("tracker"), cat = "tracker")
-violence = data.frame(domains = read_lines("violence"), cat = "violence")
+ads = data.frame(domains = read_lines("ads"), category = "ads")
+gambling = data.frame(domains = read_lines("gambling"), category = "gambling")
+finance = data.frame(domains = read_lines("finance"), category = "finance")
+porn = data.frame(domains = read_lines("porn"), category = "porn")
+spyware = data.frame(domains = read_lines("spyware"), category = "spyware")
+tracker = data.frame(domains = read_lines("tracker"), category = "tracker")
+violence = data.frame(domains = read_lines("violence"), category = "violence")
 
 # Agg
 shalla = rbind(ads, gambling, finance, porn, spyware, tracker, violence)
